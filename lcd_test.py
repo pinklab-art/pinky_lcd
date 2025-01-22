6349ea0fcde3ef3ea118814fa2a5eacb7213b0af
kyunghwan51 <kyung133851@pinkab.art>
2025-01-22 12:34:23 +0900

    add lcd test

---
+++ lcd_test.py
@@ -0,0 +1,18 @@
+from pinky_lcd import LCD
+from PIL import Image
+import time
+
+lcd = LCD()
+
+img_width, img_height = 320, 240
+background_color = (0, 255, 0)
+text_color = (0, 255, 0)
+
+img = Image.new('RGB', (img_width, img_height), color=background_color)
+img.show()
+
+lcd.img_show(img)
+
+time.sleep(3)
+
+lcd.close()
